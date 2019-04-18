d5c50f8d7bf1c55956726647670ecfcdd2eccfe5
cyr-kov <19241279+cyr-kov@users.noreply.github.com>
2019-04-18 23:11:41 +1000

    Оne Handed Bandit improved, 7 lesson

--- l7/l7app5.rb
+++ l7/l7app5.rb
@@ -12,17 +12,28 @@ if ans=="Y" and n>=	18
 		x = rand (0..9)
 		y = rand (0..9)
 		z = rand (0..9)
+		puts "Выпало #{x} #{y} #{z}"
 		if x==0 && y==0 && z==0
 			puts "Ваш баланс обнулён"
 			exit
 		end
-		1.upto(9) do |nn|
-			if x==nn && y==nn && z==nn
-				puts "Вам зачислено #{nn*10} долларов. Ваш баланс #{money}"
-			 	money=money+nn*10
+		#1.upto(9) do |nn|
+			if x==y && y==z && x!=6
+				puts "Вам зачислено #{x*10} долларов."
+			 	money+=x*10
 			end
+		#end
+		if x==y && y==z && x==6
+			money/=2
+			puts "Уполовинивание депозита"
 		end
-		money=money-1
+		if  x==1 && y==2 && z==3
+			puts "!!!!!!!!!MEGA BONUS
+			".concat(3,3,3,3,3)
+			money+=123
+		end
+		money-=1
+		puts "Ваш баланс #{money}"
 		if money<=0 
 			puts "Вы проигрались!"
 			exit
