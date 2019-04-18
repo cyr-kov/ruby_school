e9d6512005ef58ead46e55c8037d0647b70ed9b4
cyr-kov <19241279+cyr-kov@users.noreply.github.com>
2019-04-18 14:54:36 +1000

    2nd app , 7 lesson

---
+++ l7/l7app1.rb
@@ -0,0 +1,12 @@
+#encoding cp866
+
+print "Вы довольны своей зарплатой? (Y/N) "
+answer=gets.chomp.capitalize
+ if answer=="Y"
+ 	puts "Очень Хорошо"
+end
+
+if answer=="N"
+	puts "очень плохо"
+
+puts "Тут идут сложные вычисления"
