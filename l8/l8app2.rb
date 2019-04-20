0fb96aa208ed14ceee97422b14b889ea69ac2306
cyr-kov <19241279+cyr-kov@users.noreply.github.com>
2019-04-20 13:17:19 +1000

    game guess a number, 8 lesson

--- l8/l8app2.rb
+++ l8/l8app2.rb
@@ -1,16 +1,26 @@
-print "Я загадал число. Угадай какое? (от 1 до 100)"
-n=gets.to_i
-
+print "Я загадал число. Угадай какое? (от 1 до 100):"
+answer=gets.to_i
+gameWon=false
+winTries=0
 x=rand(1..100)
-1000.times do
-	if n==x 
-		puts "Угадал, молодец!"
-		exit
-	elsif x>n
+maxTries=10
+maxTries.times do |count|
+	if answer==x 
+		puts "Угадал! Молодец!"
+		winTries=count+1
+		gameWon=true
+		break
+	elsif x>answer
 		puts "Нет, больше"
 	else 
 		puts "Нет, меньше"
 	end
-	print "Ещё попытка: "
-	n=gets.to_i
+	print "Попытка №#{count+1}, Осталось попыток #{maxTries-count}: "
+	answer=gets.to_i
 end
+if gameWon
+	gameMsg="Вы выиграли за #{winTries} попыток"
+else
+	gameMsg="Вы проиграли"
+end
+puts "Игра окончена! #{gameMsg}"
